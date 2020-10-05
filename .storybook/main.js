9c3ee098441a44765fa68c082246cd3b9236009d
hanjunlee <opsdownn@gmail.com>
2020-10-05 22:18:26 +0900

    Add stroybook.

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../source/**/*.stories.mdx",
+    "../source/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
