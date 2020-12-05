aa8d953c0eb24e7132ff1b2e3223ea671741f45b
hanjunlee <opsdownn@gmail.com>
2020-12-05 22:14:53 +0900

    rm actions, reducers in redux.

--- source/core/adapters/redux/reducers/index.js
+++
@@ -1,1 +0,0 @@
-export {rootReducer} from './root'
--- source/core/adapters/redux/reducers/root.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import * as actions from '../actions';
-import {listReducer} from './list';
-import {searchReducer} from './search';
-
-export const rootReducer = combineReducers({
-  list: listReducer,
-  search: searchReducer,
-});
